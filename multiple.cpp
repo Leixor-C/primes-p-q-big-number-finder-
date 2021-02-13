b755e4a5dacaceee7e5542dded8e26ddc73603c9
Leixor-C <leixor.program@gmail.com>
2021-02-13 23:24:33 +0100

    Add files via upload

--- multiple.cpp
+++ multiple.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
-#include <cstddef>
-#include <stdlib.h>
+
+
 #include <math.h>
-static unsigned long long int profundidad=0;
+static unsigned long long int depth=0;
 static unsigned long long int mod_num=0;
 
-unsigned long long int findp(unsigned long long int num,unsigned long long int mod){
+unsigned long long int mod_rec(unsigned long long int num,unsigned long long int mod){
 	unsigned long long int sub_mod=num%mod;
-	++profundidad;
+	++depth;//looking how many iter of this fuc does
 	if (sub_mod>1){
 
-		return findp(num,sub_mod);
+		return mod_rec(num,sub_mod);
 	}
 	if(sub_mod ==1)
 		return 0;
 	return mod;
 }
-main(){
-	unsigned long long int p=2038074739;
-	unsigned long long int q=1299689;
-	unsigned long long int num=p*q;
-	unsigned long long int pmod=sqrtl(num);
+unsigned long long int findp(unsigned long long int num){
+	unsigned long long int pmod=sqrtl(num)+1;
+	unsigned long long int p;
+	while(0==(p=mod_rec(num,pmod))){
+		pmod+=12;
+		++mod_num;//looking how many times does this
 
-	unsigned long long int sub_mod;
-	while(0==(sub_mod=findp(num,pmod))){
-		pmod-=13;
-		++mod_num;
 	}
-	std::cout<<"\np:"<<sub_mod<<" q:"<<(num/sub_mod)<<" count:"<<mod_num<<" profundidad:"<<profundidad/mod_num;
+	return p;		
+}
+main(){
+	unsigned long long int A=1299689;
+	unsigned long long int B=22801763477;//55944044237;
+	unsigned long long int num=A*B;
+
+	unsigned long long int p_result =findp(num);	
+	std::cout<<"\np:"<<p_result<<" q:"<<(num/p_result)<<" count:"<<mod_num<<" depth:"<<depth/mod_num;
 }
